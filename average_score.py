b60bbb9377d0cb31ce7bf859e70191f98c760c07
Remor <tiunov.ivan99@mail.ru>
2018-09-17 11:49:14 +0300

    Подсчёт количества студентов и суммы их баллов теперь реализован через функции sum() и len(). Вывод результата через .format() улучшен

--- average_score.py
+++ average_score.py
@@ -5,18 +5,10 @@ class_list = [{'school_class': '4a', 'scores': [3, 4, 4, 5, 2]},
               {'school_class': '5b', 'scores': [2, 4, 5]}]
 score_sum = 0
 student_number = 0
-score_in_class_sum = 0
-student_in_class_number = 0
 for class_index in class_list:
-    for score_index in class_index['scores']:
-        score_in_class_summ += score_index
-        student_in_class_number += 1
-    score_sum += score_in_class_sum
-    student_number += student_in_class_number
-    print('В классе ' + class_index['school_class'] + ' средний балл - ' +
-          str(float('{0:.2f}'.format(score_in_class_sum /
-                                     student_in_class_number))))
-    score_in_class_sum = 0
-    student_in_class_number = 0
-print('Средний балл по школе - ' + str(float('{0:.2f}'.format
-                                             (score_sum / student_number))))
+    score_sum += sum(class_index['scores'])
+    student_number += len(class_index['scores'])
+    print('В классе {0} средний балл - {1:.3g}'.
+          format(class_index['school_class'],
+                 sum(class_index['scores']) / len(class_index['scores'])))
+print('Средний балл по школе - {0:.3g}'.format(score_sum / student_number))
